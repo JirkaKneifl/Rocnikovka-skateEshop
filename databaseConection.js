b5bdd456e8683254315b68228fd4e5e03ca7295b
JirkaKneifl <Jirka.kneifl@email.cz>
2021-12-11 18:22:29 +0100

    pridani poznamky

---
+++ databaseConection.js
@@ -0,0 +1,22 @@
+//pripojeni k databazi
+const mysql = require('mysql');
+
+//vytvoreni propojeni
+const conn = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'e-shop-db-prkna'
+});
+
+//funkce zda je pripojeno nebo neni
+conn.connect(function (error){
+    if(error) {
+        throw error;
+        console.log('databaze je pripojena')
+    }else{
+        console.log('databaze neni pripojena')
+    }
+});
+
+module.exports = conn;
