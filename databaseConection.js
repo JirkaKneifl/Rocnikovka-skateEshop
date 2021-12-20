b433474cf50d4a030bfc25b4a30020fa7aba5e98
JirkaKneifl <Jirka.kneifl@email.cz>
2021-12-20 19:07:17 +0100

    databaseConection.js -> okomentovani

--- databaseConection.js
+++ databaseConection.js
@@ -1,5 +1,6 @@
 var mysql = require('mysql');
 
+//vytvoreni spojeni s DB
 var spojeni = mysql.createConnection({
   host: "localhost",
   user: "root",
@@ -7,6 +8,7 @@ var spojeni = mysql.createConnection({
   database: "e-shop-db-prkna"
 });
 
+//pouziti spojeni k overeni zda jsem pripojeni
 spojeni.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
