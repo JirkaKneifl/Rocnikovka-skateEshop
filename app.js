9ecd38c775ff32439a0ae17e2b635cbc71a16632
JirkaKneifl <Jirka.kneifl@email.cz>
2021-12-30 13:05:06 +0100

    app.js -> nastaveni view enginu

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 var ZakaznickyRouter = require('./routy/register');
 const app = express();
 
+app.set('view engine', 'ejs'); //nastaveni view enginu
+
 app.use('./routy/register.js', ZakaznickyRouter);
 
 const port=8080;
