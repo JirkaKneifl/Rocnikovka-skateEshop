5d5108456acf7d4b946816156263aa0595278faf
JirkaKneifl <Jirka.kneifl@email.cz>
2022-01-03 18:39:28 +0100

    vymazani zbytečnosti v app.js

--- app.js
+++ app.js
@@ -1,31 +1,13 @@
 const express = require('express');
 const app = express();
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
 
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+//var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.set('view engine', 'ejs'); //nastaveni view enginu
 
-//routa na homepage
-app.get('/home', function (req,res){
-    res.render('home');
-    console.log("jsi na homepage")
-});
-
-//routa na registerpage
-app.get('/register', function (req,res){
-    res.render('register');
-    console.log("jsi na registerpage")
-});
-
-
-app.post('/register', urlencodedParser, function (req,res){
-    console.log(req.body);
-});
 
 
 
-
-
-const port=8080;
+const port=8080; //jaky mam port
 app.listen(port, () => console.log(`Aplikace běží na portu ${port}`));
