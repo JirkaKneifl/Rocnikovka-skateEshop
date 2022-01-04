5fd348ccc9f5734fc03cfcce98897045233c8c49
JirkaKneifl <Jirka.kneifl@email.cz>
2022-01-04 22:15:15 +0100

    zmeny ktery nefachaj

--- routes/postPages.js
+++ routes/postPages.js
@@ -1,9 +1,9 @@
-var dataDoDB = require('../controllers/register');
+var register = require('../controllers/register');
 const express = require('express');
 const router = express.Router();
 
 
 
-router.post('/register', dataDoDB);
+router.post('/register', register.DataDoDB);
 
 module.exports = router;
