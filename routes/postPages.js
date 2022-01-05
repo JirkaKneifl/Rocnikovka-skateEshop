04dd1b797e56b754e8cf909c88c086e9f16cb768
JirkaKneifl <Jirka.kneifl@email.cz>
2022-01-05 21:19:53 +0100

    pridani routy na controller ->  register.js

--- routes/postPages.js
+++ routes/postPages.js
@@ -1,9 +1,9 @@
-const register = require('../controllers/register');
+var register = require('../controllers/register');
 const express = require('express');
 const router = express.Router();
 
 
-
+//post routa na register -> postuje data funkce DataDoDB z register.js
 router.post('/register', register.DataDoDB);
 
 module.exports = router;
