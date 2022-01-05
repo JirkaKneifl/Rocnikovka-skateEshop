8dc7d32d664828120aa2054b4451755208d54ceb
JirkaKneifl <Jirka.kneifl@email.cz>
2022-01-05 21:13:25 +0100

    import dat do DB funguje + dodelani komentu

--- controllers/register.js
+++ controllers/register.js
@@ -5,16 +5,17 @@ var router = express.Router();
 
 
 function DataDoDB (req, res ){
-      const {jmeno, prijmeni, mobil, email, heslo} = req.body
-      console.log(req.body)
+      const {jmeno, prijmeni, telefon, email, heslo} = req.body //konstatnta ve které se uloží data z formu
 
           //vložíme hodnoty z formuláře do databaze
-          spojeni.query(`INSERT INTO zakaznici(jmeno, prijmeni, mobil, email, heslo) VALUES ('${jmeno}','${prijmeni}', '${mobil}','${email}','${heslo}')`, 
-          (err, result) => {
+        spojeni.query(`INSERT INTO zakaznici(jmeno, prijmeni, telefon, email, heslo) VALUES ('${jmeno}','${prijmeni}', '${telefon}','${email}','${heslo}')`, 
+        function (err){
               if(err) {
-                  console.log(err);
+                console.log(err);
               }else {
-                  return res.redirect('/registerOk');
+                console.log("Data se importovali dobře")
+                return res.redirect('/registerOk'); //redirektuju na registerOk.js
+                  
           }
       })  
     };
