d69e456ad138b1d3fb37e0788fabb4e3eace7c6a
JirkaKneifl <Jirka.kneifl@email.cz>
2022-01-05 09:35:29 +0100

    tato verze by měla byt ok

--- controllers/register.js
+++ controllers/register.js
@@ -4,13 +4,12 @@ var router = express.Router();
 
 
 
-exports.DataDoDB=(req, res)=>{
-      const {jmeno, prijmeni, mobil, email, password} = req.body
+function DataDoDB (req, res ){
+      const {jmeno, prijmeni, mobil, email, heslo} = req.body
       console.log(req.body)
 
-      
           //vložíme hodnoty z formuláře do databaze
-          spojeni.query(`INSERT INTO zakaznici(jmeno, prijmeni, mobil, email, heslo) VALUES ('${jmeno}','${prijmeni}', '${mobil}','${email}','${hashedPassword}')`, 
+          spojeni.query(`INSERT INTO zakaznici(jmeno, prijmeni, mobil, email, heslo) VALUES ('${jmeno}','${prijmeni}', '${mobil}','${email}','${heslo}')`, 
           (err, result) => {
               if(err) {
                   console.log(err);
@@ -18,7 +17,7 @@ exports.DataDoDB=(req, res)=>{
                   return res.redirect('/registerOk');
           }
       })  
-};
+    };
 
-    
+module.exports.DataDoDB = DataDoDB;   
 
