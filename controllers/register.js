d845c83debd2d64662ee81fa3ec17b5aefc217ea
JirkaKneifl <Jirka.kneifl@email.cz>
2022-01-06 10:54:26 +0100

    pridani bcrypt a async funkce na hashovani hesla do DB

--- controllers/register.js
+++ controllers/register.js
@@ -1,24 +1,30 @@
-var spojeni = require('../databaseConection');
-var express = require('express');
+const spojeni = require('../databaseConection');
+
+const express = require('express');
 var router = express.Router();
+
+const bcrypt = require('bcrypt');
+var saltRounds = 10; //kolikrat se zahashuje heslo -> 10x
 
 
 
 function DataDoDB (req, res ){
       const {jmeno, prijmeni, telefon, email, heslo} = req.body //konstatnta ve které se uloží data z formu
 
-          //vložíme hodnoty z formuláře do databaze
-        spojeni.query(`INSERT INTO zakaznici(jmeno, prijmeni, telefon, email, heslo) VALUES ('${jmeno}','${prijmeni}', '${telefon}','${email}','${heslo}')`, 
+      bcrypt.hash(heslo, saltRounds).then(function(hash) {//async funkce na zahashovani hesla
+        
+        //vložíme hodnoty z formuláře do databaze
+        spojeni.query(`INSERT INTO zakaznici(jmeno, prijmeni, telefon, email, heslo) VALUES ('${jmeno}','${prijmeni}', '${telefon}','${email}','${hash}')`, 
         function (err){
               if(err) {
                 console.log(err);
               }else {
                 console.log("Data se importovali dobře")
-                return res.redirect('/registerOk'); //redirektuju na registerOk.js
-                  
-          }
-      })  
-    };
+                return res.redirect('/registerOk'); //redirektuju na registerOk.js  
+             }
+        })
+      })
+}
 
 module.exports.DataDoDB = DataDoDB;   
 
