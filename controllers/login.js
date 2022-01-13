d22237ea71e8e81086fa9912b4ca044f1bc1dae7
JirkaKneifl <Jirka.kneifl@email.cz>
2022-01-13 20:16:25 +0100

    zprovozneni celeho loginu

--- controllers/login.js
+++ controllers/login.js
@@ -1,16 +1,26 @@
 const spojeni = require('../databaseConection');
-
 const express = require('express');
-
+const mysql = require('mysql');
 const bcrypt = require('bcrypt');
 
 
-function DataZDB(req, res){
+function DataZDB(req, res) {
+	const {email, heslo} = req.body;
+    console.log(email, heslo);
+ 
+	spojeni.query(`SELECT * FROM zakaznici WHERE email = '${email}'`, function (err, result) {
+		if(err) {
+			console.log(err);
+		}
+		console.log(result)
+		for (let i = 0; i < result.length; i++) {
+			if(email == result[i].email && bcrypt.compareSync(heslo, result[i].heslo)) {
+				console.log(heslo);
+				return res.redirect('/');
+			}
+			
+		}
+	})
+}
 
-    bcrypt.compare(myPlaintextPassword, hash).then(function(result) {
-        // result == true
-    });
-
-};
-
-module.exports = DataZDB;
+module.exports.DataZDB = DataZDB;
