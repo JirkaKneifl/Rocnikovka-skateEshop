e095e970b1a80f13c75b5bb9069327f15078da9c
JirkaKneifl <Jirka.kneifl@email.cz>
2022-01-13 20:35:59 +0100

    pridani komentu

--- controllers/login.js
+++ controllers/login.js
@@ -6,16 +6,15 @@ const bcrypt = require('bcrypt');
 
 function DataZDB(req, res) {
 	const {email, heslo} = req.body;
-    console.log(email, heslo);
  
 	spojeni.query(`SELECT * FROM zakaznici WHERE email = '${email}'`, function (err, result) {
 		if(err) {
 			console.log(err);
 		}
-		console.log(result)
+
+		//console.log("vysledek: " + result + result.length)
 		for (let i = 0; i < result.length; i++) {
-			if(email == result[i].email && bcrypt.compareSync(heslo, result[i].heslo)) {
-				console.log(heslo);
+			if(email == result[i].email && bcrypt.compareSync(heslo, result[i].heslo)) { //{result} to je objekt ketry projedu forcyklem a jeho index porovnam s emailem a heslem z imputu
 				return res.redirect('/');//navrat na homepage
 			}
 			
