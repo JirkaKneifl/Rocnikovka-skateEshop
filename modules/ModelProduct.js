8dcce44d7c692d2968d0d23fd2c3ee63bc0d0306
JirkaKneifl <Jirka.kneifl@email.cz>
2022-03-30 19:05:27 +0200

    formatovani dokumentu

--- modules/ModelProduct.js
+++ modules/ModelProduct.js
@@ -1,31 +1,30 @@
-
 function query(sql) {
-  return new Promise(function(resolve, reject){
-      try {
-          spojeni.query(sql, function(err, results){
-              if(err){
-                  return reject(err);
-              }
-              return resolve(results);
-          })
-      } catch (err) {
-          reject(err);
-      }
-  })
+  return new Promise(function (resolve, reject) {
+    try {
+      spojeni.query(sql, function (err, results) {
+        if (err) {
+          return reject(err);
+        }
+        return resolve(results);
+      });
+    } catch (err) {
+      reject(err);
+    }
+  });
 }
-
 
 //
 async function SelectDataNadKategorieProduktu() {
-  return query(`SELECT * FROM produkty WHERE ID_kategorie IN (SELECT ID_kategorie FROM kategorie WHERE ID_kat_nadrazene = '${ID_hlavniKategorie}';`)
+  return query(
+    `SELECT * FROM produkty WHERE ID_kategorie IN (SELECT ID_kategorie FROM kategorie WHERE ID_kat_nadrazene = '${ID_hlavniKategorie}';`);
 }
 
-async function SelectDataPodKategorieProduktu(){
-  return query(`SELECT * FROM produkty WHERE ID_kategorie IN (SELECT ID_kategorie FROM kategorie WHERE ID_kat_nadrazene = '${ID_podkategorie}';`)
+async function SelectDataPodKategorieProduktu() {
+  return query(
+    `SELECT * FROM produkty WHERE ID_kategorie IN (SELECT ID_kategorie FROM kategorie WHERE ID_kat_nadrazene = '${ID_podkategorie}';`);
 }
 
 module.exports = {
   SelectDataNadKategorieProduktu,
   SelectDataPodKategorieProduktu,
 };
-
