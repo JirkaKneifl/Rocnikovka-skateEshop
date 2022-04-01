82a6b11be3d32d40416eb455d325dcc0169d4ec2
JirkaKneifl <Jirka.kneifl@email.cz>
2022-04-01 16:40:16 +0200

    z poloviny funkční zobrazování výpisu produktu

--- controllers/categoryProducsController.js
+++ controllers/categoryProducsController.js
@@ -7,18 +7,21 @@ router.get('/:ID_hlavniKategorie/:ID_podkategorie?', async function (req, res){ 
     const { ID_hlavniKategorie, ID_podkategorie } = req.params;
     const NadKategorieProdukty = await ModelProduktu.SelectDataNadKategorieProduktu(parseInt(ID_hlavniKategorie));
     const PodKetegorieProdukty = await ModelProduktu.SelectDataPodKategorieProduktu(parseInt(ID_podkategorie));
+
+    const StromProduktu = await ModelProduktu.SelectDataNadKategorieProduktu(ID_hlavniKategorie, ID_podkategorie)
+
     const categoriesTree = await ModelCategory.SelectAllCategories();//ulozeni JSON objektu do categoriesTree 
     
 
     //2 query 
-    if (ID_podkategorie === undefined){
+    /*if (ID_podkategorie === undefined){
         res.render('../views/productPage/index.ejs', {NadKategorieProdukty, categoriesTree})
     }else{
         res.render('../views/productPage/index.ejs', {PodKetegorieProdukty, categoriesTree})
-    }
-
-    console.log(parseInt(ID_podkategorie))
-    console.log(req.params)
+    }*/
+    console.log(StromProduktu)
+    //console.log(parseInt(ID_podkategorie))
+    //console.log(req.params)
 })
 
 module.exports = router;
