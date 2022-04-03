642f824ecaa2d35bae3285400c775eb066d441ba
JirkaKneifl <Jirka.kneifl@email.cz>
2022-04-03 12:12:21 +0200

    pridani funkci pro selektovani dat jednoho produktu, vsech produktu dle ID, produktu podkategorie dle ID

--- modules/ModelProduct.js
+++ modules/ModelProduct.js
@@ -15,34 +15,20 @@ function query(sql) {
   });
 }
 
-async function SelectDataNadKategorieProduktu(ID_hlavniKategorie) {
-  return query(`SELECT * FROM produkty WHERE ID_kategorie IN (SELECT ID_kategorie FROM kategorie WHERE ID_kat_nadrazene = '${ID_hlavniKategorie}');`); //'${ID_hlavniKategorie}'
+async function SelectDataJednohoProduktu(ID_produktu) {
+  return query(`SELECT * FROM produkty WHERE ID_produktu = '${ID_produktu}'`);
 }
 
-async function SelectDataPodKategorieProduktu(ID_podkategorie) {
-  return query(`SELECT * FROM produkty WHERE ID_kategorie IN (SELECT ID_kategorie FROM kategorie WHERE ID_kat_nadrazene = '${ID_podkategorie}');`);//'${ID_podkategorie}'
+async function SelectVsechnyProdukty(ID_kategorie){
+   return query(`SELECT * FROM produkty WHERE ID_kategorie IN (SELECT ID_kategorie FROM kategorie WHERE ID_kat_nadrazene = '${ID_kategorie}');`);
 }
 
-async function SelectVsechnyProdukty(ID_hlavniKategorie, ID_podkategorie){
-    const produkty = await query(`SELECT * FROM produkty;`)
-    
-    return produkty.filter(Produkt => Produkt.ID_produktu === ID_hlavniKategorie).map(Produkt => ({
-        ...Produkt, 
-        PodProdukt: produkty.filter(PodProdukt => PodProdukt.ID_produktu === ID_podkategorie)
-    }))
-
-    /*
-    const categories = await query(`SELECT * FROM kategorie;`)
-    
-    return categories.filter(category => category.ID_kat_nadrazene === null).map(category => ({
-        ...category, 
-        podkategorie: categories.filter(podCategory => podCategory.ID_kat_nadrazene === category.ID_kategorie)
-    }))
-    */
+async function SelectProduktyZPodkategorie(ID_kategorie){
+  return query(`SELECT * FROM produkty WHERE ID_kategorie = '${ID_kategorie}'`);
 }
 
 module.exports = {
-  SelectDataNadKategorieProduktu,
-  SelectDataPodKategorieProduktu,
   SelectVsechnyProdukty,
+  SelectDataJednohoProduktu,
+  SelectProduktyZPodkategorie,
 };
