975042b5aa2884afe812170e06340f635a233597
JirkaKneifl <Jirka.kneifl@email.cz>
2022-04-03 12:15:16 +0200

    zprovozneni funkcí pro rendrování produktů na výpis s produkty, pro rendrování dat jednoho produktu na info o produktu

--- controllers/categoryProducsController.js
+++ controllers/categoryProducsController.js
@@ -3,25 +3,35 @@ const router = express.Router();
 const ModelProduktu = require('../modules/ModelProduct')
 const ModelCategory = require('../modules/ModelCategory');
 
+//funkce pro generovani jednotlivych produktu
+router.get('/produkty/:ID_produktu?', async function (req, res){ 
+    const categoriesTree = await ModelCategory.SelectAllCategories();//ulozeni JSON objektu do categoriesTree
+    const { ID_produktu } = req.params;
+
+    const PorduktyInfo = await ModelProduktu.SelectDataJednohoProduktu(ID_produktu);
+    res.render('../views/productPageInfo/index.ejs', {categoriesTree, PorduktyInfo})
+}) 
+
+//funkce rendrujici produkty do vypisu produktu dle dane URL id
 router.get('/:ID_hlavniKategorie/:ID_podkategorie?', async function (req, res){ // -> /k/:hlavniKategorie/:podkategorie?
+    const categoriesTree = await ModelCategory.SelectAllCategories();//ulozeni JSON objektu do categoriesTree
     const { ID_hlavniKategorie, ID_podkategorie } = req.params;
-    const NadKategorieProdukty = await ModelProduktu.SelectDataNadKategorieProduktu(parseInt(ID_hlavniKategorie));
-    const PodKetegorieProdukty = await ModelProduktu.SelectDataPodKategorieProduktu(parseInt(ID_podkategorie));
 
-    const StromProduktu = await ModelProduktu.SelectDataNadKategorieProduktu(ID_hlavniKategorie, ID_podkategorie)
+     if (ID_podkategorie === undefined) {
+       const ID_kategorie = parseInt(ID_hlavniKategorie);
 
-    const categoriesTree = await ModelCategory.SelectAllCategories();//ulozeni JSON objektu do categoriesTree 
-    
+        const Produkty = await ModelProduktu.SelectVsechnyProdukty(ID_kategorie);
+        res.render('../views/productPage/index.ejs', { categoriesTree, Produkty })
+        //console.log(Produkty);
+    } else {
+       const ID_kategorie = parseInt(ID_podkategorie);
 
-    //2 query 
-    /*if (ID_podkategorie === undefined){
-        res.render('../views/productPage/index.ejs', {NadKategorieProdukty, categoriesTree})
-    }else{
-        res.render('../views/productPage/index.ejs', {PodKetegorieProdukty, categoriesTree})
-    }*/
-    console.log(StromProduktu)
-    //console.log(parseInt(ID_podkategorie))
-    //console.log(req.params)
-})
+        const Produkty = await ModelProduktu.SelectProduktyZPodkategorie(ID_kategorie);
+        res.render('../views/productPage/index.ejs', { categoriesTree, Produkty })
+        //console.log(Produkty);
+    }
+}) 
+
+
 
 module.exports = router;
