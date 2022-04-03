9764d6a67290decbee33c081b48a68c81d4ffa79
JirkaKneifl <Jirka.kneifl@email.cz>
2022-04-03 12:12:59 +0200

    pridani komentu

--- controllers/homePage.js
+++ controllers/homePage.js
@@ -5,6 +5,8 @@ const ModelCategory = require('../modules/ModelCategory');
 router.get('/', async function (req, res){
     const categoriesTree = await ModelCategory.SelectAllCategories();//ulozeni JSON objektu do categoriesTree 
     res.render('../views/homePage/index.ejs', {categoriesTree})
+
+    //console.log(JSON.stringify(categoriesTree, undefined, 4))
 })
 
 module.exports = router;
